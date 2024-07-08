183b4f627a8d829361c71460532b4bc5994681f8
Glacoon <guillian.celle@gmail.com>
2024-07-08 23:54:48 +0200

    first commit

---
+++ atm.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const slider = document.querySelector('.sliderInfos');
+    const indicators = document.querySelectorAll('.indicator');
+    let isScrolling;
+
+    function updateIndicators(currentGroup) {
+        indicators.forEach((indicator, index) => {
+            if (index === currentGroup) {
+                indicator.classList.add('active');
+            } else {
+                indicator.classList.remove('active');
+            }
+        });
+    }
+
+    slider.addEventListener('scroll', (event) => {
+        window.clearTimeout(isScrolling);
+        isScrolling = setTimeout(function() {
+            const scrollLeft = slider.scrollLeft;
+            const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+            const groupWidth = slider.clientWidth; // Largeur d'un groupe de slides
+            const currentGroup = Math.round(scrollLeft / groupWidth);
+
+            updateIndicators(currentGroup);
+        }, 66);
+    }, false);
+
+    indicators.forEach((indicator, index) => {
+        indicator.addEventListener('click', () => {
+            slider.scrollTo({
+                left: slider.clientWidth * index,
+                behavior: 'smooth'
+            });
+            updateIndicators(index);
+        });
+    });
+});
